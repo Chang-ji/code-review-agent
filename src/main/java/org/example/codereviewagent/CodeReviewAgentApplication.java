1f904cf77dcee6f7b3cf38ba721ee1e673570eed
yuz010.kim <rladbwmd123@gmail.com>
2025-06-02 08:50:04 +0900

    first commit

---
+++ src/main/java/org/example/codereviewagent/CodeReviewAgentApplication.java
@@ -0,0 +1,13 @@
+package org.example.codereviewagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeReviewAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeReviewAgentApplication.class, args);
+    }
+
+}
